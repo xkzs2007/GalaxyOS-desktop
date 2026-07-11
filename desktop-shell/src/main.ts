d0c4be1fe8c6bc638d9a948a1a6f00bc255f1c0d
Developer <2997417176@qq.com>
2026-07-12 01:21:46 +0800

    chore: remove old desktop-shell (replaced by desktop-tauri)

--- desktop-shell/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from 'svelte';
-import '@jboltai/tokui/css';
-import './app.css';
-import App from './App.svelte';
-
-const app = mount(App, { target: document.getElementById('app')! });
-
-export default app;
